73eea55560f22feff39e7c4b81754fb3275f723b
matias <matias_ortiz_@outlook.es>
2024-06-18 09:54:48 -0300

    upload project

---
+++ app/src/main/java/com/mfo/instagramclone/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mfo.instagramclone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mfo.instagramclone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
